2d0d8a81d1c4566e399ed8d0917d262517ad7103
James Austen <jamesausten8@gmail.com>
2020-07-22 22:41:56 +0100

    initial push

---
+++ app/src/main/java/com/example/leds/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.leds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                toggle();
+            }
+        });
+    }
+
+
+    void toggle(){
+        CharSequence text = "Button Pressed";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+
+        String baseURL = "http://192.168.1.80:5000/";
+        String request = "toggle";
+        String JsonUrl = baseURL + request;
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(
+                Request.Method.GET, JsonUrl, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject obj = new JSONObject(response.toString());
+                            ArrayList<String> results = new ArrayList<>();
+
+
+                        } catch (JSONException e) {
+                            Log.d("ERROR", "onResponse: "+e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR", "Error in receiving JSON Response:\n" + error.toString());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+    }
+
+
+
+}
